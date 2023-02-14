6806f78ab7791c0e2843f701985c06de676068ac
irrinaP <121332930+irrinaP@users.noreply.github.com>
2023-02-14 12:04:48 +0700

    Update p.cs

--- p.cs
+++ p.cs
@@ -3,30 +3,20 @@ using System;
 namespace Laboratory {
   class Program {
     static void Main(string[] args) {
-      
       Console.WriteLine("Введите действительное число (x):");
-
       int x = int.Parse(Console.ReadLine());
       int workX = x;
-
-      do
-      {
+      do {
           workX = workX / 10;
-
-          if (workX < 100)
-          {
-              do
-              {
+          if (workX < 100) {
+              do {
                   workX = workX - 10;
-
               }
               while (workX > 10);
           }
-      }
-      while (workX > 100);
-
+      } while (workX > 100);
       int n = int.Parse(x.ToString().Remove(1, 1) + workX); 
-
+      
       Console.WriteLine("Результат: " + n); 
     }
   }
